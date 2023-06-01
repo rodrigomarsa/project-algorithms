1cf8a5284ca90078edde75647bbb42c99cc7b7ce
Rodrigo <rodrigomartins.agro@gmail.com>
2023-06-01 17:50:17 -0300

    feat: add study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    counter = 0
+    for initial, end in permanence_period:
+        if not isinstance(initial, int) or not isinstance(end, int):
+            return None
+        elif target_time in range(initial, end + 1):
+            counter += 1
+    return counter
